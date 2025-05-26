d53613b5aad9174cb88c40b1cfd3e39378550e35
Laurent <mazoyer.laurent@gmail.com>
2025-05-26 13:47:34 +0200

    create movieCard component and import on homePage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,27 @@
+import MovieCard from '@/components/MovieCard';
 import SearchBar from '@/components/SearchBar';
 import { icons } from '@/constants/icons';
 import { images } from '@/constants/images';
+import { fetchPopularMovies } from '@/services/api';
+import useFetch from '@/services/userFetch';
 import { useRouter } from 'expo-router';
-import { Image, ScrollView, View } from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: movies,
+    loading,
+    error,
+  } = useFetch(() => fetchPopularMovies({ query: '' }));
 
   return (
     <View className="flex-1 bg-primary">
@@ -16,12 +32,41 @@ export default function Index() {
         contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <View className="flex-1 mt-5">
-          <SearchBar
-            onPress={() => router.push('/search')}
-            placeholder="Rechercher"
+        {loading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
           />
-        </View>
+        ) : error ? (
+          <Text>Erreur : {error?.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push('/search')}
+              placeholder="Rechercher"
+            />
+            <>
+              <Text className="text-lg font-bold mt-5 mb-3 text-white">
+                Les plus récents
+              </Text>
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => <MovieCard {...item} />}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: 'flex-start',
+                  marginBottom: 10,
+                  gap: 20,
+                  paddingRight: 5,
+                }}
+                className="mt-2 pb-32"
+                scrollEnabled={false}
+              />
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
---
+++ components/MovieCard.tsx
@@ -0,0 +1,49 @@
+import { icons } from '@/constants/icons';
+import { Link } from 'expo-router';
+import React from 'react';
+import { Image, Text, TouchableHighlight, View } from 'react-native';
+
+const MovieCard = ({
+  id,
+  poster_path,
+  title,
+  vote_average,
+  release_date,
+}: Movie) => {
+  return (
+    <Link href={`/movies/${id}`} asChild>
+      <TouchableHighlight className="w-[30%]">
+        <View>
+          <Image
+            className="w-full h-52 rounded-lg"
+            resizeMode="cover"
+            source={{
+              uri: poster_path
+                ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                : 'https://placehold.co/600x400/1a1a1a/fffff.png',
+            }}
+          />
+          <Text numberOfLines={1} className="text-sm font-bold text-white mt-2">
+            {title}
+          </Text>
+          <View className="flex-row items-center justify-start gap-x-1">
+            <Image source={icons.star} className="size-4" />
+            <Text className="text-white text-xs font-bold uppercase">
+              {Math.round(vote_average / 2)}
+            </Text>
+          </View>
+          <View className="flex-row items-center justify-between">
+            <Text className="text-xs text-light-300 font-medium mt-1">
+              {release_date?.split('-')[0] || 'N/A'}
+            </Text>
+            {/* <Text className="text-xs text-light-300 font-medium uppercase">
+              Catégorie
+            </Text> */}
+          </View>
+        </View>
+      </TouchableHighlight>
+    </Link>
+  );
+};
+
+export default MovieCard;
