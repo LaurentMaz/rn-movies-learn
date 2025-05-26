ce10f367890860d9993c24e8e4982223b76beaab
Laurent <mazoyer.laurent@gmail.com>
2025-05-26 16:28:29 +0200

    Integration BDD Appwrite pour compter les recherches

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,6 +3,7 @@ import SearchBar from '@/components/SearchBar';
 import { icons } from '@/constants/icons';
 import { images } from '@/constants/images';
 import { fetchMovies } from '@/services/api';
+import { updateSearchCount } from '@/services/appwrite';
 import useFetch from '@/services/userFetch';
 import { useFocusEffect } from '@react-navigation/native';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
@@ -44,6 +45,8 @@ const Search = () => {
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await refetch();
+        if (movies?.length > 0 && movies?.[0])
+          await updateSearchCount(searchQuery, movies[0]);
       } else {
         reset();
       }
